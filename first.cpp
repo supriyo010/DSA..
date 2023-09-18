e558b163d53a96031fc0beeb1fffa752ce51d355
Supriyo maji <122034818+supriyo010@users.noreply.github.com>
2023-09-18 23:34:58 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,8 @@
+
+void push(struct Node** head_ref , int new_data)
+{
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
